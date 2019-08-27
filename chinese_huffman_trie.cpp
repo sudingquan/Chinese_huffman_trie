a85d6c591a0b7e27a895ae8bfe16cdcb59189383
sudingquan <1151015256@qq.com>
2019-08-27 20:19:20 +0800

    8-27

--- chinese_huffman_trie.cpp
+++ chinese_huffman_trie.cpp
@@ -10,6 +10,8 @@
 #include <string.h>
 #define MAX_SIZE 1000  //最大词长
 #define MAX_N 1000 //最大词数
+#define BASE 2
+#define BASE_LETTER '0'
 #define swap(a, b) { \
     __typeof(a) temp = a; \
     a = b, b = temp; \
@@ -37,13 +39,61 @@ typedef struct Data {
     double p;
 } Data;
 
-struct 
+typedef struct trie_node {
+    int flag;
+    unsigned char *word;
+    struct trie_node *next[BASE], *fail;
+} trie_node;
 
 Data arr[256];
 int data[256];
-unsigned char str[MAX_N][MAX_SIZE * 3];
+
+struct word {
+    unsigned char str[MAX_SIZE * 3];
+    char huffman_code[10000];
+} word[MAX_N];
+
 int word_cnt = 0;
 int character_cnt = 0;
+
+//queue
+typedef struct Queue {
+    trie_node **data;
+    int head, tail, size;
+} Queue;
+
+Queue *init_queue(int n) {
+    Queue *q = (Queue *)calloc(sizeof(Queue), 1);
+    q->data = (trie_node **)malloc(sizeof(trie_node *) * n);
+    q->tail = q->head = 0;
+    return q;
+}
+
+trie_node *front(Queue *q) {
+    return q->data[q->head];
+}
+
+int empty(Queue *q) {
+    return q->head == q->tail;
+}
+
+void push(Queue *q, trie_node *node) {
+    q->data[q->tail++] = node;
+    return ;
+}
+
+void pop(Queue *q) {
+    if (empty(q)) return ;
+    q->head++;
+}
+
+void clear_queue(Queue *q) {
+    if (q == NULL) return ;
+    free(q->data);
+    free(q);
+    return ;
+}
+//queue end
 
 Node *getNewNode(Data *obj) {
     Node *p = (Node *)malloc(sizeof(Node));
@@ -112,27 +162,92 @@ HuffmanTree *build(Data * arr, int n) {
     return tree;
 }
 
+trie_node *get_new_node() {
+    trie_node *p = (trie_node *)calloc(sizeof(trie_node), 1);
+    return p;
+}
+
+inline int code(char ch) { return ch - BASE_LETTER; }
+
+void insert(trie_node *root, char *str, unsigned char *word) {
+    trie_node *p = root;
+    for (int i = 0; str[i]; i++) {
+        if (p->next[code(str[i])] == NULL) p->next[code(str[i])] = get_new_node();
+        p = p->next[code(str[i])];
+    }
+    p->flag = 1;
+    p->word = (unsigned char *)malloc(sizeof(unsigned char) * MAX_SIZE * 3);
+    memcpy(p->word, word, sizeof(unsigned char) * MAX_SIZE * 3);
+    return ;
+}
+
+void build_ac(trie_node *root, int n) {
+    Queue *q = init_queue(n + 10);
+    root->fail = NULL;
+    push(q, root);
+    while (!empty(q)) {
+        trie_node *now_node = front(q);
+        pop(q);
+        for (int i = 0; i < BASE; i++) {
+            if (now_node->next[i] == NULL) continue;
+            trie_node *p = now_node->fail;
+            while (p && p->next[i] == NULL) p = p->fail;
+            if (p == NULL) now_node->next[i]->fail = root;
+            else now_node->next[i]->fail = p->next[i];
+            push(q, now_node->next[i]);
+        }
+    }
+    return ;
+}
+
+void match(trie_node *root, const char *str) {
+    trie_node *p = root;
+    for (int i = 0; str[i]; i++) {
+        int ind = str[i] - BASE_LETTER;
+        while (p && p->next[ind] == NULL) p = p->fail;
+        if (p == NULL) p = root;
+        else p = p->next[ind];
+        trie_node *q = p;
+        while (q) {
+            if (q->flag) {
+                printf("匹配到 : %s\n", q->word);
+            }
+            q = q->fail;
+        }
+    }
+    return ;
+}
+
+trie_node *build_trie(int word_cnt) {
+    trie_node *root = get_new_node();
+    for (int i = 0; i < word_cnt; i++) {
+        insert(root, word[i].huffman_code, word[i].str);
+    }
+    build_ac(root, word_cnt);
+    return root;
+}
+
 int main() {
     while (1) {
-        scanf("%s", str[word_cnt]);
-        if (strcmp((char *)str[word_cnt], "END") == 0) {
+        scanf("%s", word[word_cnt].str);
+        if (strcmp((char *)word[word_cnt].str, "END") == 0) {
             break;
         }
         word_cnt++;
     }
     printf("输入了%d个词\n", word_cnt);
     for (int i = 0; i < word_cnt; i++) {
-        for (int j = 0; str[i][j]; j++) {
-            data[str[i][j]] += 1;
+        for (int j = 0; word[i].str[j]; j++) {
+            data[word[i].str[j]] += 1;
         }
     }
+    printf("字节频次表:\n");
     for (int i = 0; i < 256; i++) {
         if (data[i] == 0) continue;
         printf("%d : %d\n", i, data[i]);
         character_cnt += 1;
     }
     printf("总字节个数:%d\n", character_cnt);
-    printf("字节频次表:\n");
     for (int i = 0, j = 0; i < 256; i++) {
         if (data[i] == 0) continue;
         arr[j].ch = i;
@@ -140,8 +255,37 @@ int main() {
         j++;
     }
     HuffmanTree *tree = build(arr, character_cnt);
+    printf("字节编码表:\n");
     for (int i = 0; i < tree->n; i++) {
         printf("%d : %s\n", tree->codes[i].ch, tree->codes[i].str);
     }
+    printf("词语编码表:\n");
+    for (int i = 0; i < word_cnt; i++) {
+        int len = strlen((char *)word[i].str);
+        for (int j = 0; j < len; j++) {
+            int x = 0;
+            while (word[i].str[j] != tree->codes[x].ch) x++;
+            strcat(word[i].huffman_code, tree->codes[x].str);
+        }
+        printf("%s : %s\n", word[i].str, word[i].huffman_code);
+    }
+    trie_node *root = build_trie(word_cnt);
+    printf("输入母串开始匹配\n");
+    unsigned char *str = (unsigned char *)malloc(sizeof(unsigned char) * 100000);
+    char *str_huffman_code = (char *)malloc(sizeof(char) * 1000000);
+    scanf("%s", str);
+    for (int i = 0; str[i]; i++) {
+        int flag = 0, j = 0;
+        for (; j < tree->n; j++) {
+            if (str[i] != tree->codes[j].ch) continue;
+            flag = 1;
+            break;
+        }
+        if (flag) {
+            strcat(str_huffman_code, tree->codes[j].str);
+        }
+    }
+    printf("%s\n", str_huffman_code);
+    match(root, str_huffman_code);
     return 0;
 }
